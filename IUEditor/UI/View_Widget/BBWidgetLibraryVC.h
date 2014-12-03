d6f51698eb938d2735a18736324e07b11a622f31
JD <j@jdlab.org>
2014-12-03 17:53:27 +0900

    Add comment on BBWidgetLibrary VC

--- IUEditor/UI/View_Widget/BBWidgetLibraryVC.h
+++ IUEditor/UI/View_Widget/BBWidgetLibraryVC.h
@@ -9,22 +9,28 @@
 #import <Cocoa/Cocoa.h>
 #import "IUBox.h"
 #import "IUDefinition.h"
+/**
+ @brief BBWidgetLibraryVC is responsible for widget selection in IUEditor
 
+ When user want to insert new IU, she will interact with view.
+ After user interaction, MainWindow calls notification, IUNoti_widgetSelectionChanged. Notification's user info has selected widget name as nsstring, with key IUNotiKey_selectedWidget. IUNotiKey_selectedWidget can be nil if user does not select any object
+
+
+ */
 @interface BBWidgetLibraryVC : NSViewController
 
+/**
+ set widgets in vc
+ @param widgets: NSArray which contains NSDictionary which contains {@"name": group name, @"image": a group image, @"widgets": an array of nsstrings, which are widget-names}
+ */
 - (void)setWidgets:(NSArray *)widgets;
 
 
-/* for unit test only */
+//////////////////////////////////////////////////
+// for unit test only
+
 @property (weak) IBOutlet NSPopUpButton *groupSelectPopupBtn;
 @property (nonatomic) NSInteger selectedGroupIndex; // selected popup index
-
-@end
-
-/**
- Following part should be used in unittest only
- */
-@interface BBWidgetLibraryVC(UnitTest)
-
+//////////////////////////////////////////////////
 
 @end
